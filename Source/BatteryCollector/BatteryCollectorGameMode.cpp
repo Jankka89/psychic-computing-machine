1c0cbccf12fba5033e162da6fb0cad9b30ac83e0
Jankka89 <jankka89@gmail.com>
2019-11-01 11:34:42 +0200

    Unreal Project Battery Collector Source Codes

---
+++ Source/BatteryCollector/BatteryCollectorGameMode.cpp
@@ -0,0 +1,164 @@
+// Copyright 1998-2019 Epic Games, Inc. All Rights Reserved.
+
+#include "BatteryCollectorGameMode.h"
+#include "BatteryCollectorCharacter.h"
+#include "UObject/ConstructorHelpers.h"
+#include "Kismet/GameplayStatics.h"
+#include "Blueprint/UserWidget.h"
+#include "SpawnVolume.h"
+#include "Components/SkeletalMeshComponent.h"
+#include "GameFramework/PawnMovementComponent.h"
+
+ABatteryCollectorGameMode::ABatteryCollectorGameMode()
+{
+
+	PrimaryActorTick.bCanEverTick = true;
+
+	// set default pawn class to our Blueprinted character
+	static ConstructorHelpers::FClassFinder<APawn> PlayerPawnBPClass(TEXT("/Game/ThirdPersonCPP/Blueprints/ThirdPersonCharacter"));
+	if (PlayerPawnBPClass.Class != NULL)
+	{
+		DefaultPawnClass = PlayerPawnBPClass.Class;
+	}
+
+	// Base decay rate
+	DecayRate = 0.01f;
+}
+
+void ABatteryCollectorGameMode::BeginPlay()
+{
+	Super::BeginPlay();
+
+	// Find all spawn volume actors
+	TArray<AActor *> FoundActors;
+	UGameplayStatics::GetAllActorsOfClass(GetWorld(), ASpawnVolume::StaticClass(), FoundActors);
+
+	for (auto Actor : FoundActors)
+	{
+		ASpawnVolume *SpawnVolumeActor = Cast<ASpawnVolume>(Actor);
+		if (SpawnVolumeActor)
+		{
+			SpawnVolumeActors.AddUnique(SpawnVolumeActor);
+		}
+	}
+
+	SetCurrentState(EBatteryPlayState::EPlaying);
+
+	// Set the score to beat
+	ABatteryCollectorCharacter *MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (MyCharacter)
+	{
+		//
+		PowerToWin = (MyCharacter->GetInitialPower()) * 1.25f;
+	}
+
+	if (HUDWidgetClass != nullptr)
+	{
+		CurrentWidget = CreateWidget<UUserWidget>(GetWorld(), HUDWidgetClass);
+		if (CurrentWidget != nullptr)
+		{
+			CurrentWidget->AddToViewport();
+		}
+	}
+}
+
+void ABatteryCollectorGameMode::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+	// Check that we are using the battery collector character
+	ABatteryCollectorCharacter *MyCharacter = Cast<ABatteryCollectorCharacter>(UGameplayStatics::GetPlayerPawn(this, 0));
+	if (MyCharacter)
+	{
+		/** If our power is greater than needed to win, set the games state to Won */
+		if (MyCharacter->GetCurrentPower() > PowerToWin)
+		{
+			SetCurrentState(EBatteryPlayState::EWon);
+		}
+
+		// if the characters power is positive
+		else if (MyCharacter->GetCurrentPower() > 0)
+		{
+			// decrease the characters power using the decay rate
+			MyCharacter->UpdatePower(-DeltaTime * DecayRate * (MyCharacter->GetInitialPower()));
+		}
+		else
+		{
+			SetCurrentState(EBatteryPlayState::EGameOver);
+		}
+	}
+}
+
+float ABatteryCollectorGameMode::GetPowerToWin() const
+{
+	return PowerToWin;
+}
+
+EBatteryPlayState ABatteryCollectorGameMode::GetCurrentState() const
+{
+	return CurrentState;
+}
+
+void ABatteryCollectorGameMode::SetCurrentState(EBatteryPlayState NewState)
+{
+	CurrentState = NewState;
+	HandleNewState(CurrentState);
+}
+
+void ABatteryCollectorGameMode::HandleNewState(EBatteryPlayState NewState)
+{
+	switch (NewState)
+	{
+		// If the game is playing
+	case EBatteryPlayState::EPlaying:
+	{
+		// Spawn volumes active
+		for (ASpawnVolume* Volume : SpawnVolumeActors)
+		{
+			Volume->SetSpawningActive(true);
+		}
+	}
+	break;
+		// If we won the game
+	case EBatteryPlayState::EWon:
+	{
+		// Spawn volumes inactive
+		for (ASpawnVolume* Volume : SpawnVolumeActors)
+		{
+			Volume->SetSpawningActive(false);
+		}
+	}
+	break;
+	// If we lost the game
+	case EBatteryPlayState::EGameOver:
+	{
+		// spawn volumes inactive
+		
+		for (ASpawnVolume* Volume : SpawnVolumeActors)
+		{
+			Volume->SetSpawningActive(false);
+		}
+		// block player input
+		APlayerController* PlayerController = UGameplayStatics::GetPlayerController(this, 0);
+		if (PlayerController)
+		{
+			PlayerController->SetCinematicMode(true, false, false, true, true);
+		}
+		// ragdoll the character
+		ACharacter* MyCharacter = UGameplayStatics::GetPlayerCharacter(this, 0);
+		if (MyCharacter)
+		{
+			MyCharacter->GetMesh()->SetSimulatePhysics(true);
+			MyCharacter->GetMovementComponent()->MovementState.bCanJump = false;
+		}
+	}
+	break;
+	// Unknown/default state
+	default:
+	case EBatteryPlayState::EUnknown:
+	{
+		// Do nothing...
+	}
+	break;
+	}
+}
